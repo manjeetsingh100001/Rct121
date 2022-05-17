3f778d757c9a42018416a9ac603b65759c4c35f2
manjeetsingh100001 <manjeetsingh100001@yahoo.com>
2022-05-17 13:31:41 +0530

    assign

---
+++ assign_1/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import Card from "./Components/Card.jsx"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>All Cards</h1>
+     <Card name="manjeet" email="manjeetnandal200@gmail.com"/>
+     <Card name="simran" email="simranPagal@gmailcom"/>
+     <Card name="manjeet ji" email="manjeetji200@gmail.com"/>
+     <Card name=" wah manjeet wah" email="manjeetwah200@gmail.com"/>
+
+    </div>
+  );
+}
+
+export default App;
